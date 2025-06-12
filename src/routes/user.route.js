b7ba75b049b7ab677486fefcf7eae43216d8a6cd
obayomiayomide <obayomiayomide2020@gmail.com>
2025-06-12 18:53:39 +0100

    logout

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/user.controller');
-const { verifyAccessToken } = require('../helpers/token.helpers');
+const userController = require("../controllers/user.controller");
+const { verifyAccessToken } = require("../helpers/token.helpers");
 
-router.post('/register', userController.register); // htpp://localhost:8080/api/users/register
-router.post('/login', userController.login); // htpp://localhost:8080/api/users/login
-router.put('/update', verifyAccessToken, userController.updateProfile);
-router.get('/profile', verifyAccessToken, userController.getProfile);
+router.post("/register", userController.register); // htpp://localhost:8080/api/users/register
+router.post("/login", userController.login); // htpp://localhost:8080/api/users/login
+router.put("/update", verifyAccessToken, userController.updateProfile);
+router.get("/profile", verifyAccessToken, userController.getProfile);
 // router.post('/logout', userController.logout);
 
 module.exports = router;
